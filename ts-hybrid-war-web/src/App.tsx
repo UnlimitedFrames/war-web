520c7fcc8dd21aec188b35d0baacc237a7bfa6d6
Limi <limi@limi.info>
2023-04-19 21:18:24 +1200

    disc

--- ts-hybrid-war-web/src/App.tsx
+++ ts-hybrid-war-web/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { TextField } from '@mui/material';
+import { Alert, Button, TextField } from '@mui/material';
 import Web from './Web';
 
 function App() {
@@ -13,15 +13,11 @@ function App() {
   }, [])
   return (
     <>
-
+      <Alert severity='info'>Simply put your API key & hit enter in and the web of the current tracked conflict will be built, takes less than 10s, Any questions please on the discord</Alert>
+      <Button href="https://discord.gg/9yYrhJx" target="_blank" variant="contained" color="success" size="large">Join the Discord</Button>
       <Web apikey={apikey} selected_nat={selectedNat} />
       <TextField focused color='success' className='apikeyinput' id="outlined-basic" label="Input API key" variant="filled" onChange={(event: any) => setApikey(event.target.value)} />
       <TextField focused color='success' id="outlined-basic" label="(Optional)Input A nation ID" variant="filled" onChange={(event: any) => setselectedNat(event.target.value)} />
-      <p>
-        What is this?
-        <br />
-        Simply put your api key in, hit enter, and wait up to 10s, a map of wars of the conflict this is tracking (currently coven) will be displayed, you can put your nation ID in to see 2 levels of your wars. flag size is related to city count, Theres alot more to come, please suggest feedback to limi on discord,
-      </p>
     </>
   )
 }
