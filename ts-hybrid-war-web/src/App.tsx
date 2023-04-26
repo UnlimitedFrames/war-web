e46d0fea17aa5ee0bb9634aad2b0182e834d14ef
Limi <limi@limi.info>
2023-04-26 13:25:14 +1200

    ea

--- ts-hybrid-war-web/src/App.tsx
+++ ts-hybrid-war-web/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <Alert severity='info'>Simply put your API key & hit enter in and the web of the current tracked conflict will be built, takes less than 10s, Any questions please on the discord</Alert>
       <Button href="https://discord.gg/bnPQvGX" target="_blank" variant="contained" color="success" size="large">Join the Discord</Button>
       <Web apikey={apikey} selected_nat={selectedNat} />
-      <TextField focused color='success' type='password' className='apikeyinput' id="outlined-basic" label="Input API key" variant="filled" onChange={(event: any) => setApikey(event.target.value)} />
+      <TextField focused color='success' type='hidden' className='apikeyinput' id="outlined-basic" label="Input API key" variant="filled" onChange={(event: any) => setApikey(event.target.value)} />
       <TextField focused color='success' id="outlined-basic" label="(Optional)Input A nation ID" variant="filled" onChange={(event: any) => setselectedNat(event.target.value)} />
     </>
   )
