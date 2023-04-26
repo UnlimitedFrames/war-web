eab6eee036acc843cfd876ea83ccd5e67dafcc26
Limi <limi@limi.info>
2023-04-26 14:48:39 +1200

    a

--- ts-hybrid-war-web/src/Web.tsx
+++ ts-hybrid-war-web/src/Web.tsx
@@ -11,7 +11,7 @@ export default function Web(props: { apikey: string | undefined, selected_nat: s
 
         body: JSON.stringify({
             query: `{
-            wars(alliance_id:[5875, 622, 1742, 7674, 9927, 4648, 9948, 8804, 5039, 8819, 4729, 5049, 7531,11232,4937],active:true,first:1000){
+            wars(alliance_id:[5875, 622, 1742, 7674, 9927, 4648, 9948, 8804, 5039, 8819, 4729, 5049, 7531,11232,4937,10177],active:true,first:1000){
             data{
     
       attacker{
@@ -41,7 +41,7 @@ export default function Web(props: { apikey: string | undefined, selected_nat: s
     })
         .then(res => res.json())
         .then(res => {
-            let warring_aas = [5875, 622, 1742, 7674, 9927, 4648, 9948, 8804, 5039, 8819, 4729, 5049, 7531, 11232, 4937]
+            let warring_aas = [5875, 622, 1742, 7674, 9927, 4648, 9948, 8804, 5039, 8819, 4729, 5049, 7531, 11232, 4937, 10177]
             let nations = []
             let nation_ids: string[] = []
             let wars = []
