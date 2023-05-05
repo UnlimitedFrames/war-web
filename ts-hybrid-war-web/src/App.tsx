f9c0826307f727e864d73592df5c8c2bb380c04f
Limi <limi@limi.info>
2023-05-05 23:44:27 +1200

    MASSIVE UPDATE

--- ts-hybrid-war-web/src/App.tsx
+++ ts-hybrid-war-web/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Alert, Button, CircularProgress, Switch, TextField } from '@mui/material';
+import { Alert, Box, Button, CircularProgress, Grid, Modal, Paper, Slider, Switch, TextField, Typography, styled } from '@mui/material';
 import web from './Web';
 
 function App() {
@@ -9,6 +9,26 @@ function App() {
   const [error, setError] = useState<string | undefined>(undefined);
   const [natOrAA, setNatOrAA] = useState(false);
   const [isLoading, setisLoading] = useState(false);
+  const [level, setLevel] = useState(1)
+  const [open, setOpen] = useState(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+
+    (typeof newValue == "number" && [1, 2, 3].includes(newValue)) && setLevel(newValue);
+  };
 
   useEffect(() => {
     apikey
@@ -17,14 +37,60 @@ function App() {
   return (
     <>
       <Alert severity='info'>Simply put your API key & hit enter in and the web of the current tracked conflict will be built, takes less than 10s, Any questions please on the discord</Alert>
-      <Button href="https://discord.gg/bnPQvGX" target="_blank" variant="contained" color="success" size="large">Join the Discord</Button>
-      <div id='network' style={{ width: "1200px", height: "700px", position: "sticky", left: "0px", top: "5px", backgroundColor: "#6da177" }}></div >
-      {isLoading && <CircularProgress color="secondary" />}
-      <TextField focused color='success' className='apikeyinput' id="outlined-basic" label="Input API key" variant="filled" onChange={(event: any) => setApikey(event.target.value)} />
-      <Switch checked={natOrAA} onChange={(event) => setNatOrAA(event.target.checked)} color="secondary" />
-      <TextField focused color='success' id="outlined-basic" label={natOrAA ? "Input a nation ID" : "Input an alliance ID"} variant="filled" value={id} onChange={(event: any) => setId(event.target.value)} />
-      <Button onClick={() => web({ apikey, id, natOrAA, setisLoading, setError })} variant='contained' > Update </Button>
+      <Box>
+        <Grid spacing={2}>
+          <Grid item >
+            <Button href="https://discord.gg/bnPQvGX" target="_blank" variant="contained" color="success" size="large">Join the Discord</Button>
+          </Grid>
+          <Grid item >
+            <Button onClick={handleOpen} color='warning' variant='contained'>Open Settings</Button>
+          </Grid>
+          <Grid item >
+            <Button onClick={() => web({ apikey, id, natOrAA, setisLoading, setError, level })} variant='contained' > Update </Button>
+          </Grid>
+        </Grid >
+      </Box>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Grid container spacing={2} >
+            <Grid item>
+              <TextField value={apikey} focused color='success' className='apikeyinput' id="outlined-basic" label="Input API key" variant="filled" onChange={(event: any) => setApikey(event.target.value)} />
+            </Grid>
+            <Grid item>
+              <Paper>
+                <Switch checked={natOrAA} onChange={(event) => setNatOrAA(event.target.checked)} color="secondary" />
+                <TextField focused color='success' id="outlined-basic" label={natOrAA ? "Input a nation ID" : "Input an alliance ID"} variant="filled" value={id} onChange={(event: any) => setId(event.target.value)} />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper>
+                <Typography>
+                  Set The war Depth (More levels = More wars)
+                </Typography>
+                <Slider value={level} marks min={1} max={3} valueLabelDisplay="on" onChange={handleChange} />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Button onClick={handleClose}>Close</Button>
+            </Grid>
+            <Grid item>
+              <Button onClick={() => {
+                web({ apikey, id, natOrAA, setisLoading, setError, level })
+                handleClose()
+              }} variant='contained' > Update </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Modal>
       {error && <Alert severity='error'>{error}</Alert>}
+      {isLoading && <CircularProgress color="secondary" />}
+      <div id='network' style={{ width: "1200px", height: "700px", position: "sticky", left: "0px", top: "5px", backgroundColor: "#6da177" }}></div >
+
     </>
   )
 }
--- ts-hybrid-war-web/src/Web.tsx
+++ ts-hybrid-war-web/src/Web.tsx
@@ -1,7 +1,38 @@
 import { DataSet } from "vis-data"
 import { Network } from "vis-network"
 
-interface webProps { apikey: string, id: string, natOrAA: boolean, setisLoading: any, setError: any }
+
+type warWithWars = {
+    attacker: warNatWithWars,
+    defender: warNatWithWars,
+    turns_left: number,
+    id: string
+}
+type war = {
+    attacker: warNat,
+    defender: warNat,
+    turns_left: number
+    id: string
+}
+type warNatWithWars = {
+    nation_name: string,
+    id: string,
+    num_cities: number,
+    alliance: alliance
+    wars?: any[]
+}
+type warNat = {
+    nation_name: string,
+    id: string,
+    num_cities: number,
+    alliance: alliance
+}
+type alliance =
+    {
+        id: string,
+        flag: string
+    } | null
+interface webProps { apikey: string, id: string, natOrAA: boolean, setisLoading: any, setError: any, level: number }
 export default function web(props: webProps) {
     props.setisLoading(true)
     props.setError(undefined)
@@ -18,8 +49,96 @@ export default function web(props: webProps) {
         },
 
         body: JSON.stringify({
-            query: `{
-        wars(`+ (props.natOrAA ? ("nation_id") : ("alliance_id")) + `:[` + id + `],active:true,first:1000){
+            query: props.level == 1 ? level1(props) : props.level == 2 ? level2(props) : level3(props)
+        })
+    })
+        .then(res => res.json())
+        .then(res => {
+            res.errors &&
+                props.setError(res.errors[0].message)
+            let totalWars: war[] = []
+            res.data.wars.data.forEach(
+                (war: warWithWars) => {
+                    war.attacker?.wars && war.attacker.wars.forEach((war2: warWithWars) => {
+                        war2.attacker?.wars && war2.attacker.wars.forEach((war3: warWithWars) => {
+                            totalWars.push(war3)
+                        })
+                        war2.defender?.wars && war2.defender.wars.forEach((war3: warWithWars) => {
+                            totalWars.push(war3)
+                        })
+                        totalWars.push(war2)
+
+                    })
+                    war.defender?.wars && war.defender.wars.forEach((war2: warWithWars) => {
+                        war2.attacker.wars && war2.attacker.wars.forEach((war3: warWithWars) => {
+                            totalWars.push(war3)
+                        })
+                        war2.defender?.wars && war2.defender.wars.forEach((war3: warWithWars) => {
+                            totalWars.push(war3)
+                        })
+
+                        totalWars.push(war2)
+
+                    }
+                    )
+                    totalWars.push(war)
+                }
+            )
+
+            let nations: any[] = []
+            let wars: any[] = []
+
+
+
+            loadWars(nations, wars, totalWars)
+
+            var nodes = new DataSet(nations)
+            var edges = new DataSet(wars);
+
+
+            var data = {
+                nodes: nodes,
+                edges: edges,
+
+            };
+
+
+            var options = {};
+            new Network(container as HTMLElement, data as any, options)
+            props.setisLoading(false)
+        })
+}
+
+function loadWars(nations: any, wars: any, data: any) {
+    let nation_ids: string[] = []
+    let allowed_ids: string[] = []
+    let doneWars: string[] = []
+    data.forEach((war: war) => {
+        if (!doneWars.includes(war.id) && war.attacker.alliance != null && war.defender.alliance != null) {
+            if (allowed_ids.length == 0 || allowed_ids.includes(war.attacker.id) || allowed_ids.includes(war.defender.id)) {
+                if (!nation_ids.includes(war.attacker.id)) {
+                    nations.push({ id: war.attacker.id, weight: war.attacker.num_cities, title: war.attacker.id, scaling: { min: 5, max: 35 }, value: war.attacker.num_cities, group: war.attacker.alliance.id, label: war.attacker.nation_name, shape: "circularImage", image: war.attacker.alliance.flag })
+                    nation_ids.push(war.attacker.id)
+                }
+
+                if (!nation_ids.includes(war.defender.id)) {
+                    nations.push({ id: war.defender.id, weight: war.attacker.num_cities * 10, title: war.defender.id, scaling: { min: 5, max: 35 }, value: war.defender.num_cities, group: war.defender.alliance.id, label: war.defender.nation_name, shape: "circularImage", image: war.defender.alliance.flag })
+                    nation_ids.push(war.defender.id)
+                }
+
+                wars.push({ title: "war", arrows: 'middle', smooth: { type: "continuous" }, from: war.attacker.id, to: war.defender.id, id: war.id, width: war.turns_left / 12 })
+                doneWars.push(war.id)
+
+            }
+        }
+    })
+    return ({ nations, wars, nation_ids, allowed_ids })
+}
+
+
+function level1(props: webProps) {
+    return (`{
+        wars(`+ (props.natOrAA ? ("nation_id") : ("alliance_id")) + `:[` + props.id + `],active:true,first:1000){
         data{
 
   attacker{
@@ -41,58 +160,246 @@ defender{
   }
 }
 turns_left
+id
 }    
 }
 }
-`
-        })
-    })
-        .then(res => res.json())
-        .then(res => {
-
-            res.errors &&
-                props.setError(res.errors[0].message)
-
-
-            let nations = []
-            let nation_ids: string[] = []
-            let wars = []
-            let allowed_ids: string[] = []
-
-
-            for (let war of res.data.wars.data) {
-                if (war.attacker.alliance != null && war.defender.alliance != null) {
-                    if (allowed_ids.length == 0 || allowed_ids.includes(war.attacker.id) || allowed_ids.includes(war.defender.id)) {
-                        if (!nation_ids.includes(war.attacker.id)) {
-                            nations.push({ id: war.attacker.id, weight: war.attacker.num_cities, title: war.attacker.id, scaling: { min: 5, max: 35 }, value: war.attacker.num_cities, group: war.attacker.alliance.id, label: war.attacker.nation_name, shape: "circularImage", image: war.attacker.alliance.flag })
-                            nation_ids.push(war.attacker.id)
-                        }
-
-                        if (!nation_ids.includes(war.defender.id)) {
-                            nations.push({ id: war.defender.id, weight: war.attacker.num_cities * 10, title: war.defender.id, scaling: { min: 5, max: 35 }, value: war.defender.num_cities, group: war.defender.alliance.id, label: war.defender.nation_name, shape: "circularImage", image: war.defender.alliance.flag })
-                            nation_ids.push(war.defender.id)
-                        }
-
-                        wars.push({ title: "war", arrows: 'middle', smooth: { type: "continuous" }, from: war.attacker.id, to: war.defender.id, id: war.id, width: war.turns_left / 12 })
-
-
-                    }
+  `
+    )
+}
+function level2(props: webProps) {
+    return (`{
+    wars(`+ (props.natOrAA ? ("nation_id") : ("alliance_id")) + `:[` + props.id + `], active: true, first: 1000) {
+      data {
+        attacker {
+          nation_name
+          id
+          num_cities
+          alliance {
+            id
+            flag
+          }
+          wars(active: true){
+            attacker {
+          nation_name
+          id
+          num_cities
+          alliance {
+            id
+            flag
+          }
+        }
+        defender {
+          nation_name
+          id
+          num_cities
+          alliance {
+            id
+            flag
+          }
+        }
+        turns_left
+        id
+          }
+        }
+        defender {
+          nation_name
+          id
+          num_cities
+          alliance {
+            id
+            flag
+          }
+          wars(active: true){
+            attacker {
+          nation_name
+          id
+          num_cities
+          alliance {
+            id
+            flag
+          }
+        }
+        defender {
+          nation_name
+          id
+          num_cities
+          alliance {
+            id
+            flag
+          }
+        }
+        turns_left
+        id
+          }
+        }
+        turns_left
+        id
+      }
+    }
+  }
+  `
+    )
+}
+function level3(props: webProps) {
+    return (`{
+        wars(`+ (props.natOrAA ? ("nation_id") : ("alliance_id")) + `:[` + props.id + `], active: true, first: 1000) {
+            data {
+            attacker {
+              nation_name
+              id
+              num_cities
+                      wars(active: true) {
+              attacker {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
                 }
+                wars(active: true){
+                  attacker {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+              }
+              defender {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+              }
+              turns_left
+              id
+                }
+              }
+              defender {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+                wars(active: true){
+                  attacker {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+              }
+              defender {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+              }
+              turns_left
+              id
+                }
+              }
+              turns_left
+              id
             }
-
-            var nodes = new DataSet(nations)
-            var edges = new DataSet(wars);
-
-
-            var data = {
-                nodes: nodes,
-                edges: edges,
-
-            };
-
-
-            var options = {};
-            new Network(container as HTMLElement, data as any, options)
-            props.setisLoading(false)
-        })
+              alliance {
+                id
+                flag
+              }
+            }
+            defender {
+              nation_name
+              id
+              num_cities
+                      wars (active: true){
+              attacker {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+                wars(active: true){
+                  attacker {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+              }
+              defender {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+              }
+              turns_left
+              id
+                }
+              }
+              defender {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+                wars(active: true){
+                  attacker {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+              }
+              defender {
+                nation_name
+                id
+                num_cities
+                alliance {
+                  id
+                  flag
+                }
+              }
+              turns_left
+              id
+                }
+              }
+              turns_left
+              id
+            }
+              alliance {
+                id
+                flag
+              }
+            }
+            turns_left
+            id
+          }
+        }
+      }      
+  `
+    )
 }
